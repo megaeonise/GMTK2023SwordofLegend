cc9852bf8834f382d8bd713375f7159eeeb688fe
megaeonise <54655406+megaeonise@users.noreply.github.com>
2023-07-08 23:49:04 +0600

    Added a bunch of sword logic

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 signal mode_change(mode)
-var speed = 350
+var speed = 200
 #regular = -1, chained = 1
 var mode = -1
 var gravity = 200
-var grip = 100
-var on_ground = true
+var grip = 0
+var on_ground = false
 
+var length_up = 1
+var grip_up = 1
+var chain_up = 1
 
+func _ready():
+	add_collision_exception_with(get_node('Sword'))
 
 func _physics_process(delta):
 	velocity = Vector2(0,+gravity)
@@ -21,31 +26,34 @@ func sword_move():
 	var mouse = get_global_mouse_position()
 	var distance_to_mouse = mouse-character
 	var local_mouse = get_local_mouse_position()
-	print(local_mouse)
 	
 	if not on_ground:
 		if grip>0:
 			grip-=1
-		print(grip)
 		
 	if grip==0:
 		gravity = 200
 	
+	#GRIP IS SET HERE!!!
 	if is_on_floor():
-		grip = 100
+		grip = 25
 		on_ground = true
 	
-	if Input.is_action_just_pressed("Right_Click"):
+	if Input.is_action_just_pressed("Right_Click") and on_ground==true:
 		get_node('Sword').set_position(Vector2(0,0))
 		mode*=-1
 		mode_change.emit(mode)
-		
+	
+	if mode==1:
+		get_node('HeroSprite').play('cower')
+	
 	if mode==-1:
 		if abs(distance_to_mouse.x)>=10:
 			if Input.is_action_pressed('Left_Click'):
-				if mouse.y<position.y and grip>0:
+				if mouse.y<position.y-100 and grip>0:
 					on_ground = false
 					gravity = 0
+					get_node('HeroSprite').play('hang')
 					if local_mouse.y<-150:
 						var direction = (mouse - position).normalized()
 						velocity = (direction * speed)
@@ -54,20 +62,24 @@ func sword_move():
 						velocity = (direction * speed * 0.6)
 				else:
 					if abs(local_mouse.x)>200:
-						get_node('HeroSprite').play('run')
+						if on_ground:
+							get_node('HeroSprite').play('drag')
 						var direction = (mouse - position).normalized()
-						velocity = (direction * speed * 1.25)
+						velocity.x = (direction.x * speed * 1.00)
 					elif abs(local_mouse.x)>110:
-						get_node('HeroSprite').play('run')
+						if on_ground:
+							get_node('HeroSprite').play('run')
 						var direction = (mouse - position).normalized()
-						velocity = (direction * speed * 1.0)
+						velocity.x = (direction.x * speed * 0.75)
 					else:
-						get_node('HeroSprite').play('walk')
+						if on_ground:
+							get_node('HeroSprite').play('walk')
 						var direction = (mouse - position).normalized()
-						velocity = (direction * speed * 0.5)
+						velocity.x = (direction.x * speed * 0.5)
 			if not Input.is_action_pressed("Left_Click"):
-				get_node('HeroSprite').play('stand')
-				if not on_ground:
+				if on_ground:
+					get_node('HeroSprite').play('stand')
+				elif not on_ground:
 					gravity = 200
 
 func direction_finder():
--- Characters/Sword.gd
+++ Characters/Sword.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var sword_mode = -1
 var speed = 1000
+var radius = 50
+
 
 
 func _physics_process(delta):
@@ -13,15 +15,26 @@ func _physics_process(delta):
 func sword_move():
 	if sword_mode==1:
 		get_node('SwordCollision').set_disabled(false)
-		var character = get_position()
-		var mouse = get_parent().get_local_mouse_position()
-		var distance_to_mouse = mouse-character
-		print(character, mouse)
-		if sqrt(abs(mouse.x-character.x*mouse.x-character.x) + abs(mouse.y-character.y*mouse.y-character.y))<=200:
-			if abs(distance_to_mouse.x)>=10:
-				print(distance_to_mouse.x)
-				var direction = (mouse - position).normalized()
-				velocity = (direction * speed)
+#		var hero = get_parent().position
+#		var global_hero = get_parent().global_position
+#		var global_sword = global_position
+#		var mouse = get_parent().get_local_mouse_position()
+		var mouse_pos = get_global_mouse_position()
+		var player_pos = get_parent().global_position
+		var distance = player_pos.distance_to(mouse_pos) 
+		var mouse_dir = (mouse_pos-player_pos).normalized()
+		if distance > radius:
+			mouse_pos = player_pos + (mouse_dir * radius)
+		self.global_transform.origin = mouse_pos
+##		var distance_to_mouse = mouse-sword
+##		if global_sword.distance_to(global_hero)<=300:
+#		if mouse.distance_to(position)>=10:
+#			var direction = (mouse-position).normalized()
+#			velocity = (direction * speed)
+#			print('radius:',radius,' position distance to', global_position.distance_to(global_hero), 'hero:', global_hero)
+##			if global_sword.distance_to(global_hero)>radius:
+##				position = (direction*radius)
+
 
 	else:
 		get_node('SwordCollision').set_disabled(true)
