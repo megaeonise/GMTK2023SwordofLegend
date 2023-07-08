da5c81a3bb224a0dd82eac9c70a16e37e9d48230
voltusaur <138964613+voltusaur@users.noreply.github.com>
2023-07-09 01:45:19 +0530

    Fixed sword circle movement

--- Characters/Sword.gd
+++ Characters/Sword.gd
@@ -14,32 +14,18 @@ func _physics_process(delta):
 
 func sword_move():
 	if sword_mode==1:
-		get_node('SwordCollision').set_disabled(false)
-#		var hero = get_parent().position
-#		var global_hero = get_parent().global_position
-#		var global_sword = global_position
-#		var mouse = get_parent().get_local_mouse_position()
 		var mouse_pos = get_global_mouse_position()
-		var player_pos = get_parent().global_position
+		var player_pos = get_parent().global_transform.origin 
 		var distance = player_pos.distance_to(mouse_pos) 
 		var mouse_dir = (mouse_pos-player_pos).normalized()
 		if distance > radius:
 			mouse_pos = player_pos + (mouse_dir * radius)
-		self.global_transform.origin = mouse_pos
-##		var distance_to_mouse = mouse-sword
-##		if global_sword.distance_to(global_hero)<=300:
-#		if mouse.distance_to(position)>=10:
-#			var direction = (mouse-position).normalized()
-#			velocity = (direction * speed)
-#			print('radius:',radius,' position distance to', global_position.distance_to(global_hero), 'hero:', global_hero)
-##			if global_sword.distance_to(global_hero)>radius:
-##				position = (direction*radius)
+		global_transform.origin = mouse_pos
+		if position.y > player_pos.y:
+			position.y = player_pos.y+5
 
 func sword_attack():
 	pass
-
-	else:
-		get_node('SwordCollision').set_disabled(true)
 
 
 func _on_player_mode_change(mode):
