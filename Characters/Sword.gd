c98e36db87a6b6caca495efc50afd2cd38a32e89
megaeonise <54655406+megaeonise@users.noreply.github.com>
2023-07-08 21:28:34 +0600

    hahahah added movement

--- Characters/Sword.gd
+++ Characters/Sword.gd
@@ -1,21 +1,30 @@
 extends CharacterBody2D
 
 var sword_mode = -1
-var speed = 200
+var speed = 1000
+
 
 func _physics_process(delta):
+	var mouse = get_parent().get_local_mouse_position()
 	velocity = Vector2(0,0)
 	sword_move()
 	move_and_slide()
 
 func sword_move():
 	if sword_mode==1:
-		var character = get_parent().get_position()
+		get_node('SwordCollision').set_disabled(false)
+		var character = get_position()
 		var mouse = get_parent().get_local_mouse_position()
+		var distance_to_mouse = mouse-character
 		print(character, mouse)
-		if sqrt(abs(mouse.x-character.x*mouse.x-character.x) + abs(mouse.y-character.y*mouse.y-character.y))<=500:
-			print(sqrt(abs(mouse.x-character.x*mouse.x-character.x) + abs(mouse.y-character.y*mouse.y-character.y)))
-			position = mouse
+		if sqrt(abs(mouse.x-character.x*mouse.x-character.x) + abs(mouse.y-character.y*mouse.y-character.y))<=200:
+			if abs(distance_to_mouse.x)>=10:
+				print(distance_to_mouse.x)
+				var direction = (mouse - position).normalized()
+				velocity = (direction * speed)
+
+	else:
+		get_node('SwordCollision').set_disabled(true)
 
 
 func _on_player_mode_change(mode):
